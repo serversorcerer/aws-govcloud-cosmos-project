58b11d6d6e4e163c74f18cfa6bfcd4c1ca77b80a
serversorcerer <joe@josephaleto.iom>
2025-06-23 02:11:02 -0400

    💥 Force overwrite: Replace remote with local clean state

--- archive/terraform/modules/ipam/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "gov_pool_id" {
-  description = "The ID of the GovCloud simulated IPAM pool"
-  value       = aws_vpc_ipam_pool.gov_pool.id
-}
--- archive/terraform/modules/vpc/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "vpc_id" {
-  description = "ID of the created VPC"
-  value       = aws_vpc.gov_vpc.id
-}
-
-output "cidr_block" {
-  description = "CIDR block assigned to the VPC"
-  value       = aws_vpc.gov_vpc.cidr_block
-}
